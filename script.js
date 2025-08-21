09dd13b9df96d4cc3a0cee7117c6bdfffe8870db
Sufyan Hassan <sufyan2321@gmail.com>
2025-08-21 23:14:26 +0100

    Updated skeleton. MVP achieved.

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addButton = document.getElementById("addButton");
+    const deleteButton = document.getElementById("deleteButton");
+    const list = document.querySelector(".list");
+    const input = document.getElementById("input1");
 
+    addButton.addEventListener("click", () => {
+        const text = input.value.trim();
+
+        if (text != "") {
+            const newTask = document.createElement("div");
+            newTask.innerHTML = text;
+            newTask.classList.add("task");
+
+            newTask.addEventListener("click", () => {
+                newTask.classList.toggle("clicked")
+            })
+
+            list.appendChild(newTask);
+        }
+    })
+
+    deleteButton.addEventListener("click", () => {
+        const selected = document.querySelectorAll(".task.clicked");
+        selected.forEach(task => {
+            task.remove();
+        })
+    })
+
+
+})   
